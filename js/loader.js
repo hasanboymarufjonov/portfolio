8a8b03725793833f173cc8948dd8ca0050529cc5
Hasanboy Marufjonov <theRealHasanboy@gmail.com>
2021-04-13 10:24:34 +0500

    updated

--- js/loader.js
+++ js/loader.js
@@ -7,5 +7,4 @@ function myFunction() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
 }
--- js/navbar.js
+++ js/navbar.js
@@ -1,38 +1,20 @@
-const sections = document.querySelectorAll('section');
-const bubble = document.querySelector('.bubble');
+let nav = document.querySelector('nav');
+let dropdown = nav.querySelector('.dropdown');
+let dropdownToggle = nav.querySelector("[data-action='dropdown-toggle']");
+let navToggle = nav.querySelector("[data-action='nav-toggle']");
 
+dropdownToggle.addEventListener('click', () => {
+	if (dropdown.classList.contains('show')) {
+		dropdown.classList.remove('show');
+	} else {
+		dropdown.classList.add('show');
+	}
+})
 
-const options = {
-    threshold: 0.7
-};
-
-let observer = new IntersectionObserver(navCheck, options);
-
-function navCheck(entries){
-    entries.forEach(entry =>{
-        const className = entry.target.className; 
-        const activeAnchor = document.querySelector(`[data-page=${className}]`);
-        const gradientIndex = entry.target.getAttribute('data-index');
-        const coords = activeAnchor.getBoundingClientRect();
-        const directions = {
-            height: coords.height,
-            width: coords.width,
-            top: coords.top,
-            left: coords.left
-        };
-        if (entry.isIntersecting){
-           bubble.style.setProperty('left', `${directions.left}px`); 
-           bubble.style.setProperty('top', `${directions.top}px`); 
-           bubble.style.setProperty('width', `${directions.width}px`); 
-           bubble.style.setProperty('height', `${directions.height}px`); 
-           } 
-    });
-}
-
-sections.forEach(section => {
-    observer.observe(section);
-});
-
-
-
-
+navToggle.addEventListener('click', () => {
+	if (nav.classList.contains('opened')) {
+		nav.classList.remove('opened');
+	} else {
+		nav.classList.add('opened');
+	}
+})
